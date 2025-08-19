117e3f29b2a1bf39a9bb0202f5f04e62d214d362
Giovanna Borges Coelho <10756784@educa.mackenzie.br>
2025-08-19 10:16:51 -0300

    finalizando exercicio

--- Aula_1/exercicio_1.java
+++ Aula_1/exercicio_1.java
@@ -1,5 +1,5 @@
 /* Em uma indústria, foram coletados os dados de consumo elétrico (em kWh) de 10 setores. O setor de engenharia
- * deseja identificar quantos setores
+ * deseja identificar quantos setores apresentam consumo anômalo
  */
 
 package Aula_1;
@@ -10,7 +10,19 @@ public class exercicio_1 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
 
-        double [] consumo = new double[5];      //vetor criado
-        
+        double [] consumos = new double[5];      //vetor criado
+        for (int i = 0; i < consumos.length; i++) {
+            System.out.printf("Consumo %d: ", (i + 1));
+            consumos [i] = entrada.nextDouble();
+        }
+
+        int qtde = 0;
+        for (double consumo : consumos) {   //cria uma variável consumo para armazenar cada conteúdo do índice de consumos
+            if (consumo > 5000) {
+                System.out.printf("Consumo anômalo: %.1f\n", consumo);
+                qtde++;
+            }
+        }
+        System.out.printf("Quantidade de consumo anômalo: %d\n", qtde);
     }
 }
