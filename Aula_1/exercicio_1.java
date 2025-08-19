19e130823825ae29379af622f2c88c3691f6eb11
Giovanna Borges Coelho <10756784@educa.mackenzie.br>
2025-08-19 09:54:00 -0300

    primeiro commit

---
+++ Aula_1/exercicio_1.java
@@ -0,0 +1,16 @@
+/* Em uma indústria, foram coletados os dados de consumo elétrico (em kWh) de 10 setores. O setor de engenharia
+ * deseja identificar quantos setores
+ */
+
+package Aula_1;
+
+import java.util.Scanner;
+
+public class exercicio_1 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        double [] consumo = new double[5];      //vetor criado
+        
+    }
+}
